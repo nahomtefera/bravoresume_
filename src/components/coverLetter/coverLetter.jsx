bff9ce0f637707f2705f93cc7cf2af3c36420b8c
nahom <nahumtefera@gmail.com>
2018-09-27 22:51:50 -0700

    added construction icon, and under construction message on cover letter component, changed nav links style

--- src/components/coverLetter/coverLetter.jsx
+++ src/components/coverLetter/coverLetter.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import './coverLetter.css'
 
 export default class CoverLetter extends Component {
 
     render() {
        return (
         <div className="cover-letter-container">
-            Cover Letter
+    
+            <div className="cover-letter-construction">
+                <h2 className="construction-title">Feature is under construction, be pacient</h2>
+                <img className="construction-image" src={require('../images/construction.png')} alt=""/>
+            </div>
+    
         </div>
        )
     }
