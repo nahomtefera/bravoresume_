eba09c7868a1231eea70f3c3231bb22089b6ab0a
nendale <nahom_endale@gap.com>
2019-01-26 16:29:07 -0800

    added option to sign-in using email and password

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
       ],
       callbacks: {
         signInSuccessWithAuthResult: (data) => {
